2ed0c17a6a7c5f9d69feb25d15318e34e8c2e1b8
koczoa <onokoczo@protonmail.com>
2026-04-23 08:38:25 +0200

    basic time measurements

--- simulate.py
+++ simulate.py
@@ -1,6 +1,6 @@
 from os.path import join
 import sys
-
+import time
 import numpy as np
 
 
@@ -65,10 +65,22 @@ if __name__ == '__main__':
     MAX_ITER = 20_000
     ABS_TOL = 1e-4
 
+
+    times = []
+
+
     all_u = np.empty_like(all_u0)
     for i, (u0, interior_mask) in enumerate(zip(all_u0, all_interior_mask)):
+
+        select_start = time.perf_counter()
+
         u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
+
+        times.append(time.perf_counter() - select_start)
+
         all_u[i] = u
+
+
 
     # Print summary statistics in CSV format
     stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15']
@@ -76,3 +88,6 @@ if __name__ == '__main__':
     for bid, u, interior_mask in zip(building_ids, all_u, all_interior_mask):
         stats = summary_stats(u, interior_mask)
         print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
+
+    print("time stats")
+    print(np.average(np.array(times)))
